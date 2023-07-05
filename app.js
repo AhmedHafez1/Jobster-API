99098c47adbe5122906b2f169feec3cbf3bf2338
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2023-07-05 07:17:24 +0300

    cleaning

--- app.js
+++ app.js
@@ -3,14 +3,7 @@ require('express-async-errors');
 
 // extra security packages
 const helmet = require('helmet');
-const cors = require('cors');
 const xss = require('xss-clean');
-const rateLimiter = require('express-rate-limit');
-
-// Swagger
-const swaggerUI = require('swagger-ui-express');
-const YAML = require('yamljs');
-const swaggerDocument = YAML.load('./swagger.yaml');
 
 const express = require('express');
 const app = express();
@@ -24,22 +17,9 @@ const jobsRouter = require('./routes/jobs');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
-app.set('trust proxy', 1);
-app.use(
-  rateLimiter({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // limit each IP to 100 requests per windowMs
-  })
-);
 app.use(express.json());
 app.use(helmet());
-app.use(cors());
 app.use(xss());
-
-app.get('/', (req, res) => {
-  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
-});
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // routes
 app.use('/api/v1/auth', authRouter);
