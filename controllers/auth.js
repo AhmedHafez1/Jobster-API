feb7114af9975f28e911a0012bc59d37870225af
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2023-07-08 12:11:01 +0300

    Update User

--- controllers/auth.js
+++ controllers/auth.js
@@ -43,7 +43,31 @@ const login = async (req, res) => {
   });
 };
 
+const updateUser = async (req, res) => {
+  const { email, name, lastName, location } = req.body;
+  if (!email || !name || !lastName || !location)
+    throw new BadRequestError('Provide all user data!');
+
+  const user = await User.findOne({ _id: req.user.userId });
+  user.email = email;
+  user.name = name;
+  user.lastName = lastName;
+  user.location = location;
+
+  const token = await user.createJWT();
+  res.status(StatusCodes.OK).json({
+    user: {
+      email: user.email,
+      lastName: user.lastName,
+      location: user.location,
+      name: user.name,
+      token,
+    },
+  });
+};
+
 module.exports = {
   register,
   login,
+  updateUser,
 };
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,9 @@
-const express = require('express')
-const router = express.Router()
-const { register, login } = require('../controllers/auth')
-router.post('/register', register)
-router.post('/login', login)
+const express = require('express');
+const auth = require('../middleware/authentication');
+const router = express.Router();
+const { register, login, updateUser } = require('../controllers/auth');
+router.post('/register', register);
+router.post('/login', login);
+router.patch('/updateUser', auth, updateUser);
 
-module.exports = router
+module.exports = router;
