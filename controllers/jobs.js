ff2d95267331dbf7b0081f469c01ce372e38bc1c
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2023-07-17 12:49:18 +0300

    jobs pagination

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -22,9 +22,22 @@ const getAllJobs = async (req, res) => {
   if (sort === 'a-z') sortProb = 'position';
   if (sort === 'z-a') sortProb = '-position';
 
-  const jobs = await Job.find(queryObject).sort(sortProb);
-  res.status(StatusCodes.OK).json({ jobs, count: jobs.length });
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  const jobs = await Job.find(queryObject)
+    .sort(sortProb)
+    .skip(skip)
+    .limit(limit);
+
+  const totalJobs = await Job.countDocuments(queryObject);
+  const numOfPages = Math.ceil(totalJobs / limit);
+  res
+    .status(StatusCodes.OK)
+    .json({ jobs, count: jobs.length, totalJobs, numOfPages });
 };
+
 const getJob = async (req, res) => {
   const {
     user: { userId },
