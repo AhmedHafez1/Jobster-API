021de45c749d0f09899b1ebfb9c023b43acac018
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2023-07-18 07:05:55 +0300

    API Limiter

--- routes/auth.js
+++ routes/auth.js
@@ -3,9 +3,16 @@ const auth = require('../middleware/authentication');
 const testUser = require('../middleware/test-user');
 const router = express.Router();
 const { register, login, updateUser } = require('../controllers/auth');
+const rateLimiter = require('express-rate-limit');
 
-router.post('/register', register);
-router.post('/login', login);
+const apiLimiter = rateLimiter({
+  windowMs: 15 * 60 * 1000,
+  max: 1,
+  message: { msg: 'Too many requests in the past 15 minutes' },
+});
+
+router.post('/register', apiLimiter, register);
+router.post('/login', apiLimiter, login);
 router.patch('/updateUser', auth, testUser, updateUser);
 
 module.exports = router;
