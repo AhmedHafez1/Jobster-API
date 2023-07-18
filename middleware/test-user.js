bf68caf37d28e9e11526244b7a7f8031da422993
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2023-07-18 06:52:19 +0300

    fix bug

--- middleware/test-user.js
+++ middleware/test-user.js
@@ -1,7 +1,7 @@
 const { NotFoundError } = require('../errors');
 
 const testUser = (req, res, next) => {
-  if (req.testUser)
+  if (req.user.testUser)
     throw new NotFoundError('Demo user can not perform this action');
   next();
 };
